8550f0c09dc722225b137fc42cf7d928f66fe099
Reshma0152 <bhanus1552@gmail.com>
2025-05-03 21:11:04 +0530

    Updated file with required git  release notes for app.java

--- app.java
+++ app.java
@@ -1,2 +1,23 @@
 Hello! 
 Welcome
+
+Git v2.49 Release Notes
+=======================
+
+UI, Workflows & Features
+------------------------
+
+ * Completion script updates for zsh
+
+ * "git pack-objects" and its wrapper "git repack" learned an option
+   to use an alternative path-hash function to improve delta-base
+   selection to produce a packfile with deeper history than window
+   size.
+
+ * "git gc" learned the "--expire-to" option and passes it down to
+   underlying "git repack".
+
+ * "[help] autocorrect = 1" used to be a way to say "please wait for
+   0.1 second after suggesting a typofix of the command name before
+   running that command"; now it means "yes, if there is a plausible
+   typofix for the command name, please run it immediately".
